e43e6a7e8d32a338ae5e193615f04767f07a8c76
Barry Alienware <yuanhaozuogenius@gmail.com>
2026-09-02 03:48:40 +0800

    Publish personalized clock web app

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "personalized-clock-v3";
+const APP_FILES = ["./", "./index.html", "./styles.css", "./app.bundle.js", "./manifest.webmanifest", "./icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => event.request.mode === "navigate" ? caches.match("./index.html") : undefined))
+  );
+});
