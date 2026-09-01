7418420823282d30a1e6f3fd75f36d23f7e125a2
Barry Alienware <yuanhaozuogenius@gmail.com>
2026-09-02 04:02:31 +0800

    Bust stale reminder cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "personalized-clock-v4";
-const APP_FILES = ["./", "./index.html", "./styles.css", "./app.bundle.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "personalized-clock-v5";
+const APP_FILES = ["./", "./index.html", "./styles.css", "./app.bundle.js?v=5", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)));
